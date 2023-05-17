9d91efb913ed34d572313dcde4008307100470fd
[nayoungkim] <[kny200208@naver.com]>
2023-05-17 17:10:36 +0900

    update questionPage

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import {ProgressBar, Button} from 'react-bootstrap';
+import { QuestionData } from '../assets/data/questiondata';
 
 
 const Question=() =>{
-    return(
-    <>
-        <div>질문화면입니다.</div>
-    </>
-    )
+    return( <Wrapper>
+        <ProgressBar striped variant="danger" now={80} style={{marginTop:'20px'}}/>
+        <Title>나는 문제입니다.</Title>
+        <ButtonGroup>
+            <Button style={{width:"40%", minHeight:"200px",fontSize:"15pt"}}>나는 답변1번입니다.</Button>
+            <Button style={{width:"40%", minHeight:"200px",fontSize:"15pt",marginLeft:"20px"}}>나는 답변2번입니다.</Button>
+        </ButtonGroup>
+    </Wrapper>)
 }
 
 export default Question;
+
+
+
+const Wrapper = styled.div`
+    height:100vh;
+    width:100%;
+`
+
+const Title=styled.div`
+    font-size:30pt;
+    text-align:center;
+    font-family:"Dovemayo_wild";
+
+
+`
+const ButtonGroup=styled.div`
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+    font-family:"Dovemayo_wild";
+`
