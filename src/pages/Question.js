067ccdb74723c730ccd613690a6875fe4e5c4df3
[nayoungkim] <[kny200208@naver.com]>
2023-05-17 23:17:24 +0900

    add question module

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,21 +1,126 @@
 import React from 'react';
 import styled from 'styled-components';
 import {ProgressBar, Button} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { QuestionData } from '../assets/data/questiondata';
 
 
+
 const Question=() =>{
-    return( <Wrapper>
-        <ProgressBar striped variant="danger" now={80} style={{marginTop:'20px'}}/>
-        <Title>{QuestionData}[0].title</Title>
-        <ButtonGroup>
-            <Button style={{width:"40%", minHeight:"200px",fontSize:"15pt"}}>{QuestionData}[0].answer</Button>
-            <Button style={{width:"40%", minHeight:"200px",fontSize:"15pt",marginLeft:"20px"}}>{QuestionData}[0].answerb</Button>
-        </ButtonGroup>
+    const [questionNo,setQuestionNo]=React.useState(0);
+    const [totalScore,setTotalScore]=React.useState([
+        {id:"EI",score:0},
+        {id:"SN",score:0},
+        {id:"TF",score:0},
+        {id:"JP",score:0},
+    ])
+    const navigate=useNavigate();
+    console.log('totalScore',totalScore);
+
+    const handleClickButton = (no, type) => {
+        const newScore = totalScore.map((s) =>
+                s.id===type ? {id:s.id,score:s.score+no} : s
+        );
+    
+        setTotalScore(newScore);
+        if(QuestionData.length!==questionNo+1){
+            setQuestionNo(questionNo+1);
+        }else{
+            navigate("/result");
+        }
+          
+    
+        /*if(type==="EI"){
+             //기존 스코어에 더할 값을 계산(기존의 값+배점)
+            const addScore = totalScore[0].score+no;
+            //새로운 객체
+            const newObject={id:"EI",score:addScore};
+            //splice 통해 새로운 객체를 해당객체 자리에 넣어준다
+            totalScore.splice(0,1,newObject);
+            
+        }else if(type==="SN"){
+            
+                
+               const addScore = totalScore[0].score+no;
+               const newObject={id:"SN",score:addScore};
+               totalScore.splice(1,1,newObject);
+               
+
+            
+        }else if(type==="TF"){
+            
+
+               const addScore = totalScore[0].score+no;
+               const newObject={id:"TF",score:addScore};
+               totalScore.splice(2,1,newObject);
+             
+
+        }else if(type==="JP"){
+
+               const addScore = totalScore[0].score+no;
+               const newObject={id:"JP",score:addScore};
+               totalScore.splice(3,1,newObject);
+              
+
+        }
+       
+        setQuestionNo(questionNo+1);     
+    }
+
+    const handleClickButtonB =(no,type) =>{
+        if(type==="EI"){
+            //기존 스코어에 더할 값을 계산(기존의 값+배점)
+           const addScore = totalScore[0].score+no;
+           //새로운 객체
+           const newObject={id:"EI",score:addScore};
+           //splice 통해 새로운 객체를 해당객체 자리에 넣어준다
+           totalScore.splice(0,1,newObject);
+           
+       }else if(type==="SN"){
+           
+               
+              const addScore = totalScore[0].score+no;
+              const newObject={id:"SN",score:addScore};
+              totalScore.splice(1,1,newObject);
+              
+
+           
+       }else if(type==="TF"){
+           
+
+              const addScore = totalScore[0].score+no;
+              const newObject={id:"TF",score:addScore};
+              totalScore.splice(2,1,newObject);
+            
+
+       }else if(type==="JP"){
+
+              const addScore = totalScore[0].score+no;
+              const newObject={id:"JP",score:addScore};
+              totalScore.splice(3,1,newObject);
+             
+
+       }*/
+       
+       
+    }
+
+    return( 
+        <Wrapper>
+            <ProgressBar striped variant="danger" now={(questionNo/QuestionData.length)*100} style={{marginTop:'20px'}}/>
+            <Title>{QuestionData[questionNo].title}</Title>
+            <ButtonGroup>
+                <Button onClick={()=>handleClickButton(1,QuestionData[questionNo].type)}style={{width:"40%", minHeight:"200px",fontSize:"15pt"}}>{QuestionData[questionNo].answer}</Button>
+                <Button onClick={()=>handleClickButton(0,QuestionData[questionNo].type)}style={{width:"40%", minHeight:"200px",fontSize:"15pt",marginLeft:"20px"}}>{QuestionData[questionNo].answerb}</Button>
+            </ButtonGroup>
     </Wrapper>)
 }
 
-export default Question;
+
+    
+
+
+
 
 
 
@@ -38,3 +143,5 @@ const ButtonGroup=styled.div`
     justify-content:center;
     font-family:"Dovemayo_wild";
 `
+
+export default Question;
